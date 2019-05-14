f484d0c9db4d0e0b89fe349013b5bc7a63368867
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-05-14 16:41:51 -0300

    Fix bugs, socket worked

--- server.h
+++ server.h
@@ -7,24 +7,34 @@
 #include <sys/socket.h> 
 #include <stdlib.h> 
 #include <string.h> 
+#include <pthread.h>
 
 using namespace std;
+#define PORT 8080
 
 class Servidor
 {
     public:
- 	struct sockaddr_in address;
+ 	    struct sockaddr_in address;
         int port;
-        int server_fd, new_socket, valread;
-    	int opt = 1; 
-   	int addrlen = sizeof(address);  
-       
+        int new_socket;
+        int server_fd;
+        int opt = 1; 
+   	    int addrlen = sizeof(address);  
+        
+
+
 
 
     void createSocket();
     void listeningSocket();
-    void sendResponse(FILE *file);
+    static void functionSystemFile(char  command[1024], int socket){
+		printf("testando %s\n",command ); 
+        send(socket , command , strlen(command) , 0 ); 
 
+    }
 };
+
+
 
 
